a04b3eea07dbe5ca265fdcbf1174fd4a77a5bef9
digetl <dave@digetl.co.uk>
2020-06-24 13:33:17 +0100

    test customerHasName - failed

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,19 @@
+import Customers.Customer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+    Customer customer1;
+
+    @Before
+    public void before(){
+        customer1 = new Customer ("Janine", "07545", 5000);
+    }
+
+    @Test
+    public void customerHasName(){
+        assertEquals("Janine", customer.getName);
+    }
 }
